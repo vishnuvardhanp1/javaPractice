c112a041797af1c7830ca0cb1db03f3244a0a3b5
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-05-11 08:39:22 +0530

    Day 34 serilaization,transient

--- src/day35/Student_G.java
+++ src/day35/Student_G.java
@@ -9,10 +9,12 @@ public class Student_G implements Serializable{
 	private static final long serialVersionUID = 1L;
 	int rollNo;
 	String name;
-	public Student_G(int rollNo, String name) {
+	transient char grade;
+	public Student_G(int rollNo, String name,char grade) {
 		super();
 		this.rollNo = rollNo;
 		this.name = name;
+		this.grade=grade;
 	}
 	
 }
